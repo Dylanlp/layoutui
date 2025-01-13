241c0e6e0f95182160511e51b97b573f71ebe599
Dylanlp <68389971+Dylanlp@users.noreply.github.com>
2025-01-13 11:28:46 +0000

    update

--- apps/www/registry/new-york/example/stars-demo.tsx
+++ apps/www/registry/new-york/example/stars-demo.tsx
@@ -1,5 +1,12 @@
-import { Stars } from "@/registry/new-york/ui/stars"
+import { StarImage, Stars } from "@/registry/new-york/ui/stars"
 
 export default function StarsDemo() {
-  return <Stars count={400} text="Trusted by" />
+  return (
+    <Stars count={400} text="Trusted by">
+      <StarImage src="/faces/9.jpg" />
+      <StarImage src="/faces/2.jpg" />
+      <StarImage src="/faces/3.jpg" />
+      <StarImage src="/faces/28.jpg" />
+    </Stars>
+  )
 }
